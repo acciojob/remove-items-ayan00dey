700074fec2c426f1d1274e3a1330ddeb38116d47
ayan00dey <mr.dey.ayanava@gmail.com>
2025-03-24 23:32:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener('click',function() {
+	let colorIndex = document.getElementById("colorSelect");
+	let selectIndex = colorIndex.selectedIndex;
+	if(selectIndex !==-1){
+		colorIndex.remove(selectIndex);
+	}
+})
